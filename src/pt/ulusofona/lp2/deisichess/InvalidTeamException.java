347c85711cebcd514b1c2238c6a1c29fa5437754
alcantranetom <alcantranetomanuel@hotmail.com>
2024-01-21 19:50:32 +0000

    Atualização para a Parte 2 - (21/01/2024)

--- src/pt/ulusofona/lp2/deisichess/InvalidTeamException.java
+++ src/pt/ulusofona/lp2/deisichess/InvalidTeamException.java
@@ -1,4 +1,12 @@
 package pt.ulusofona.lp2.deisichess;
 
 public class InvalidTeamException extends Exception{
+
+
+
+
+    public String getInvalidPieceName(){
+        return null;
+    }
+
 }
--- src/pt/ulusofona/lp2/deisichess/StatType.java
+++ src/pt/ulusofona/lp2/deisichess/StatType.java
@@ -2,5 +2,10 @@ package pt.ulusofona.lp2.deisichess;
 
 public enum StatType {
 
+    TOP_5_CAPTURAS,
+    TOP_5_PONTOS,
+    PECAS_MAIS_5_CAPTURAS,
+    PECAS_MAIS_BARALHADAS,
+    TIPOS_CAPTURADOS
 
 }
