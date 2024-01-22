b61c26e2ac963f66112e3476ada5bcb8a807aec8
alcantranetom <alcantranetomanuel@hotmail.com>
2024-01-22 18:14:43 +0000

    Atualização para a Parte 2 - (21/01/2024)

---
+++ src/pt/ulusofona/lp2/deisichess/Statistics.kt
@@ -0,0 +1,58 @@
+package pt.ulusofona.lp2.deisichess
+
+class Statistics {
+
+
+    fun getStatsCalculator(statType: StatType): (GameManager) -> List<String> {
+        when (statType) {
+            StatType.TOP_5_CAPTURAS -> return ::getTop5Capturas
+            StatType.TOP_5_PONTOS -> return ::getTop5Pontos
+            StatType.PECAS_MAIS_5_CAPTURAS -> return ::getPeca5Capturas
+            StatType.PECAS_MAIS_BARALHADAS -> return ::getPecaMaisBaralhada
+            StatType.TIPOS_CAPTURADOS -> return ::getTiposCapturados
+        }
+
+    }
+
+    fun getTop5Capturas(gameManager: GameManager): ArrayList<String> {
+        val myList = ArrayList<String>()
+        myList.add("val")
+        myList.add("norma")
+        myList.add("dd")
+        return myList
+    }
+
+    fun getTop5Pontos(gameManager: GameManager): ArrayList<String> {
+
+        val myList = ArrayList<String>()
+        myList.add("val");
+        myList.add("norma")
+        myList.add("dd")
+        return myList
+    }
+
+    fun getPeca5Capturas(gameManager: GameManager): ArrayList<String> {
+        val myList = ArrayList<String>()
+        myList.add("val");
+        myList.add("norma")
+        myList.add("dd")
+        return myList
+    }
+
+    fun getPecaMaisBaralhada(gameManager: GameManager): ArrayList<String> {
+        val myList = ArrayList<String>()
+        myList.add("val");
+        myList.add("norma")
+        myList.add("dd")
+        return myList
+    }
+
+    fun getTiposCapturados(gameManager: GameManager): ArrayList<String> {
+
+        val myList = ArrayList<String>()
+        myList.add("val");
+        myList.add("norma")
+        myList.add("dd")
+        return myList
+    }
+}
